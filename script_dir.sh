2aa4a2a48d16fc0298d256936458ee949cc4b8ca
malongshuai <mlongshuai@gmail.com>
2021-06-27 08:05:32 +0800

    d

--- script_dir.sh
+++
@@ -1,9 +0,0 @@
-########  function `script_dir`: get the scirpt dirname ########
-
-#### function script_dir start ####
-function script_dir(){
-  # for i in "${BASH_SOURCE[@]}";do echo $i; done
-  dirname "$(realpath "${BASH_SOURCE[-1]}")"
-}
-export -f script_dir
-#### function script_dir end ####
